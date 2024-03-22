52bd955c22e40a4276bda7f21fa2bac022a11bbc
kenniele <kenniele@yandex.com>
2024-03-22 17:12:15 +0300

    Завершение двусвязных списков, добавление новых сортировок

--- Структуры данных/dlinked_list.py
+++ Структуры данных/dlinked_list.py
@@ -16,6 +16,14 @@ class DLinkedList:  # Двусвязный список
     def __str__(self):
         return f"{self.head}"
 
+    def info(self): # Выводит информацию о списке
+        curr = self.head
+        while curr is not None:
+            print(f"Значение - {curr.data}\n"
+                  f"{curr.prev.data if curr.prev else None} -> X -> "
+                  f"{curr.next.data if curr.next else None}\n")
+            curr = curr.next
+
     def push_back(self, data):  # Добавление элемента в конец списка O(1)
         nd = Node(data)
         self.tail.next = nd
@@ -67,3 +75,72 @@ class DLinkedList:  # Двусвязный список
             el = el.next
             i += 1
         return el
+
+    def __reversed__(self):  # Разворот списка O(n)
+        self.head, self.tail = self.tail, self.head
+        curr = self.head
+        while True:
+            curr.next, curr.prev = curr.prev, curr.next
+            if curr.next is None:
+                break
+            curr = curr.next
+
+    def join(self, other):  # Слияние двусвязных списков O(n + m)
+        if isinstance(other, DLinkedList):
+            if self.head.data < other.head.data:
+                temp_head = self.head
+                c1, c2 = self.head.next, other.head
+            else:
+                temp_head = other.head
+                c1, c2 = self.head, other.head.next
+            c = temp_head
+            while c1 is not None and c2 is not None:
+                if c1.data < c2.data:
+                    c.next = c1
+                    c1 = c1.next
+                else:
+                    c.next = c2
+                    c2 = c2.next
+                c.next.prev = c
+                c = c.next
+            while c1 is not None:
+                c.next = c1
+                c1 = c1.next
+                c.next.prev = c
+            while c2 is not None:
+                c.next = c2
+                c2 = c2.next
+                c.next.prev = c
+        return NotImplemented
+
+    def is_palindrome(self):  # Проверка, является ли палиндромом O(n)
+        c1, c2 = self.head, self.tail
+        while True:
+            if c1.data != c2.data:
+                return False
+            if c1.next is None:
+                return True
+            c1, c2 = c1.next, c2.prev
+
+    def unique(self): # Убирает повторения в списке O(n)
+        c, n = self.head, self.head.next
+        d = {c.data: 1}
+        while n is not None:
+            item = d.get(n.data, None)
+            if item is None:
+                d[n.data] = 1
+                c, n = n, n.next
+            else:
+                n = n.next
+                c.next = n
+                n.prev = c
+        self.tail = c
+
+    def get_middle(self): # Средний элемент O(n)
+        c1, c2 = self.head, self.tail
+        while c1 is not None:
+            if c1 is c2:
+                return c1.data
+            elif c1.next is c2 and c2.prev is c1:
+                return c1.data, c2.data
+            c1, c2 = c1.next, c2.prev
